8e9ff5964ca3a1a33b755c9302be81512336b0f7
tudorraul <tudorraul@gmail.com>
2015-04-13 23:18:04 +0100

    users store all posts in inverse chronological order

---
+++ src/test/scala/com/tudor/csn/UserSpec.scala
@@ -0,0 +1,32 @@
+package com.tudor.csn
+
+import akka.actor.ActorSystem
+import akka.testkit.{TestActorRef, TestKit}
+import com.tudor.csn.Commands.Post
+
+class UserSpec(_system: ActorSystem) extends SpecBundle(_system) {
+  def this() = this(ActorSystem("UserSpec"))
+
+  val userRef = TestActorRef[User](User.props("user1"))
+  val user = userRef.underlyingActor
+
+  override def afterAll() {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "User" must {
+    "store all posts in inverse chronological order" in {
+      user.messages.size should be(0)
+      val p1: Post = Post("user1", "some message 1", 1)
+      val p2: Post = Post("user1", "some message 2", 2)
+      val p3: Post = Post("user2", "some message 3", 3)
+      userRef ! p1
+      userRef ! p2
+      userRef ! p3
+      awaitCond(user.messages.size == 3)
+      user.messages.head should be(p3)
+      user.messages.tail.head should be(p2)
+      user.messages.tail.tail.head should be(p1)
+    }
+  }
+}
